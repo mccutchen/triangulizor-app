360c3110b2d11cc96a5940d0a7357b7bb1298bc7
Will McCutchen <wm@bitly.com>
2012-06-27 12:07:31 -0400

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+from ext.gaefab import *
+
+def pre_deploy_hook(tag, export):
+    """Ensure that the secrets.py file is copied in from a safe place before
+    deploying.
+    """
+    if not os.path.exists('secrets.py'):
+        local('scp overloaded.org:triangulizor-app/secrets.py .')
+    return True
+
+deploy.pre_deploy_hook = pre_deploy_hook
