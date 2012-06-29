d3a01a19b4c616616061451fae2812325590d1bd
Will McCutchen <wm@bitly.com>
2012-06-29 15:50:15 -0400

    Better error handling via sessions/flash messages.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from cStringIO import StringIO
 import json
 import logging
+import pprint
 import os
 
 from google.appengine.api import urlfetch
 import jinja2
 import webapp2
+import webapp2_extras.sessions
 
 from ext import triangulizor
+
+import secrets
 
 
 class BaseHandler(webapp2.RequestHandler):
@@ -18,9 +22,23 @@ class BaseHandler(webapp2.RequestHandler):
         loader = jinja2.FileSystemLoader(template_dir)
         self.jinja_env = jinja2.Environment(loader=loader)
 
+    def dispatch(self, *args, **kwargs):
+        self.session_store = webapp2_extras.sessions.get_store(
+            request=self.request)
+        try:
+            super(BaseHandler, self).dispatch(*args, **kwargs)
+        finally:
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        return self.session_store.get_session()
+
     def render_string(self, template_path, ctx):
+        final_ctx = self.get_default_context()
+        final_ctx.update(ctx)
         template = self.jinja_env.get_template(template_path)
-        return template.render(ctx)
+        return template.render(final_ctx)
 
     def render(self, template_path, ctx=None, status=200):
         body = self.render_string(template_path, ctx or {})
@@ -35,6 +53,12 @@ class BaseHandler(webapp2.RequestHandler):
         self.response.set_status(status)
         self.response.out.write(body)
 
+    def get_default_context(self):
+        return {
+            'flashes': self.session.get_flashes(),
+            'pformat': pprint.pformat,
+        }
+
 
 class IndexHandler(BaseHandler):
     def get(self):
@@ -47,13 +71,44 @@ class ImageCollectionHandler(BaseHandler):
 
     def post(self):
         url = self.request.get('url')
-        tile_size = int(self.request.get('tile_size') or 0)
+        if not url:
+            return self.error('Missing URL.')
+
+        try:
+            tile_size = int(self.request.get('tile_size') or 0)
+        except ValueError:
+            return self.error('Tile size must be a number or left blank.')
+
         logging.info('Fetching %s', url)
-        resp = urlfetch.fetch(url)
-        logging.info('Got ~%dKB of data', len(resp.content) / 1024)
+        try:
+            resp = urlfetch.fetch(url)
+        except urlfetch.InvalidURLError:
+            return self.error('Invalid URL.')
+
+        image_size = len(resp.content)
+        content_type = resp.headers.get('Content-Type')
+        logging.info('Got ~%dKB of %s data', image_size / 1024, content_type)
+
+        if resp.status_code != 200:
+            msg = 'Error fetching URL: HTTP %s', resp.status_code
+            return self.error(msg)
+
+        if image_size > self.app.config['max_file_size']:
+            msg = 'Max image size is %dKB (got %dKB)' % (
+                self.app.config['max_file_size'] / 1024, image_size / 1024)
+            return self.error(msg)
+
         image = triangulizor.triangulize(StringIO(resp.content), tile_size)
         self.response.headers['Content-Type'] = 'image/png'
         image.save(self.response.out, 'png')
+
+    def error(self, msg):
+        self.session.add_flash(msg, 'error')
+        ctx = {
+            'url': self.request.get('url'),
+            'tile_size': self.request.get('tile_size'),
+        }
+        return self.render('index.html', ctx, status=400)
 
 
 class ImageHandler(BaseHandler):
@@ -63,6 +118,10 @@ class ImageHandler(BaseHandler):
 
 config = {
     'template_dir': os.path.join(os.path.dirname(__file__), 'templates'),
+    'max_file_size': 1024 * 1000 * 1, # 1MB
+    'webapp2_extras.sessions': {
+        'secret_key': secrets.secret_key,
+    },
 }
 
 urls = [
